d9ebe2b9ef348cd0abbd01551baa66618a6a3e8e
srv.sngh <saurav.kumar@hashedin.com>
2014-06-27 13:37:09 +0530

    done with view1 and 2 layout working

--- DOIT/WebContent/app/js/app.js
+++ DOIT/WebContent/app/js/app.js
@@ -11,10 +11,21 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-  $routeProvider.when('/view1', {templateUrl: 'partials/view1.html', controller: 'MyCtrl1'});
+  //$locationProvider.html5Mode(true);
+  $routeProvider.when('/view1', {
+      templateUrl: 'partials/view1.html',
+      controller: 'ProjectDetails'
+    }).
+    when('/view1/:id', {
+      templateUrl: 'partials/view2.html',
+      controller: 'ProjectList'
+    }).
+    otherwise({
+      redirectTo: '/view1'
+    });
 }]);
 
 angular.module('myApp').constant('appSettings', {
   AppURI: 'http://127.0.0.1:9080/'
 });
+
--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -3,9 +3,91 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-     $scope.message = "Hello This message is from View 1";
+  .controller('ProjectDetails', ['$scope', function($scope) {
+     $scope.projectlist = [
+                           {
+                               "id": 1,
+                               "description": "Productidentifier",
+                               "name": "Assignment",
+                               "coordinators":['saurav','priyanka']
+                           },
+                           {
+                               "description": "Nameoftheproduct",
+                               "id": 2,
+                               "name": "Assignment1",
+                              "coordinators":['saurav','kousick']   
+                           },
+                           {
+                               "description": "Nameoftheproduct",
+                               "id": 3,
+                               "name": "Assignment2",
+                            	"coordinators":["kousick","priyanka"]
+                           },
+                           {
+                               "description": "Nameoftheproduct",
+                               "id": 4,
+                               "name": "Assignment3",
+                            	"coordinators":['saurav','priyanka','kousick',]
+                           }
+                       ];
+     
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('ProjectList', ['$scope', '$routeParams', 
+                              function($scope, $routeParams)  {
     $scope.message = "Hello This message is from View 2";
-  }]);
+    
+    $scope.projectlist1 = [
+                          {
+                              "id": 1,
+                              "status_task":[{"status":"pending","assign_to":"saurav","des":"sdsadcadsdsadsa","due_date":"20/5/2014",
+                                  "created_date":"20/5/2014"},{"status":"complete","des":"vdjajdsajhsjssajcvshacvcbsajbcascbdsa","due_date":"20/5/2014",
+                                      "created_date":"20/5/2014","assign_to":"saurav"}],
+                             
+                              "coordinators":[{"name":"priyanka","role":"dev"},{"name":"saurav","role":"dev"}],
+                             
+                               
+                          },
+                          {
+                              
+                              "id": 2,
+                              "status_task":[{"status":"complete", "due_date":"20/5/2014",
+                                  "created_date":"20/5/2014","des":"sdsadsadcnskcnsakcnsakcsaakc","assign_to":"saurav"},{"status":"pending","des":"streteFBskncsakacnSJXSXdsadsa", "due_date":"20/5/2014",
+                                      "created_date":"20/5/2014","assign_to":"kosick"}],
+                             "coordinators":[{"name":"kousick","role":"dev"},{"name":"saurav","role":"dev"}] ,
+                            
+                             
+                          },
+                          {
+                              
+                              "id": 3,
+                              "status_task":[{"status":"pending","assign_to":"priyanka","des":"sdsadsadckncksncskansaakcnsaaksdsadsa","due_date":"20/5/2014",
+                                  "created_date":"20/5/2014"},{"status":"pending","des":"vcsjbxsajbxjsabxjsxbxsjxsjxbsjbxsdsadsa","due_date":"20/5/2014",
+                                      "created_date":"20/5/2014","assign_to":"kosick"}],
+                           	"coordinators":[{"name":"kousick","role":"dev"},{"name":"priyanka","role":"dev"}],
+                           	
+                          },
+                          {
+                              
+                              "id": 4,
+                              "status_task":[{"status":"pending","des":"sdsadncskcnskcsakcnascksacnsaksadsdsadsa","due_date":"20/5/2014",
+                                  "created_date":"20/5/2014","assign_to":"priyanka"},{"status":"pending","des":"sajcbsajcbsajcbsajsajcbsaajcbsawesadsa","due_date":"20/5/2014",
+                                      "created_date":"20/5/2014","assign_to":"kosick"}],
+                           	"coordinators":[{"name":"kousick","role":"dev"},{"name":"priyanka","role":"dev"},{"name":"saurav","role":"dev"}],
+                           	"due_date":"20/5/2014",
+                            
+                          
+                          }
+                      ];
+    for(var i = 0; i <  $scope.projectlist1.length; i++) {
+        if($routeParams.id ==  $scope.projectlist1[i].id)
+        	{
+        $scope.projectdetails= $scope.projectlist1[i];
+        
+        
+        	}
+    }
+      
+    }]);
+
+
+
